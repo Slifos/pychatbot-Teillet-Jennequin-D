ec69f8a45f15e5acdaff55d3a695f47f6af95b68
thibault <ttteillet2005@gmail.com>
2023-11-17 13:19:46 +0100

    hello

--- functions.py
+++ functions.py
@@ -9,3 +9,4 @@ if est_sous_chaine(chaine_principale, sous_chaine_a_verifier):
     print(f"{sous_chaine_a_verifier} est une sous-chaîne de {chaine_principale}")
 else:
     print(f"{sous_chaine_a_verifier} n'est pas une sous-chaîne de {chaine_principale}")
+    #test
