fcb869ecc63806fda4b6e7d685feb6112d94cf4f
thibault <ttteillet2005@gmail.com>
2023-11-17 13:19:24 +0100

    hello

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import os
 dirPath = r"speeches"
 result = [f for f in os.listdir(dirPath) if os.path.isfile(os.path.join(dirPath, f))]
 print(result)
-print("helloooo1oo")
+nom = ['Chirac' , 'Giscard dEstaing' , 'Hollande' , 'Macron' , 'Mitterand', 'Sarkozy']
+for i in range(len(result)):
+    for j in range(len(result[0]))
