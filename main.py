8380ff8b5b5ac94a07ac66641254bd90942ef21c
SIMONTIME <xkayqi@gmail.com>
2023-11-23 16:46:14 +0100

    new version 2

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import os
-from functions import *
 
 dirPath = r"speeches"
 result = [f for f in os.listdir(dirPath) if os.path.isfile(os.path.join(dirPath, f))]
 print(result)
-nom = ['Chirac', 'Giscard dEstaing', 'Hollande', 'Macron', 'Mitterrand', 'Sarkozy']
+nom = ['Chirac' , 'Giscard dEstaing' , 'Hollande' , 'Macron' , 'Mitterand', 'Sarkozy']
 for i in range(len(result)):
-    for elt in nom:
-        if elt in result[i]:
-            result[i] = elt
-print(result)
+    for j in range(len(result[0])):
+        print("lol")
